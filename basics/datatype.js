7aadddf01603c8fc147162bd4f5431f7483dd150
bordaklal <bordaklal@gmail.com>
2025-01-30 14:35:56 +0000

    data tpes

---
+++ basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict" //treat all js code as newer version
+ //alert(3+3) //refrence error in node because we are using node js not browser  
+
+ // code readability shoud be high
+
+ //Type Of Data
+
+ //1. premitive
+    //NUmber = 2 to power of 53 range of number
+    //bigint
+    //String => ""
+    //boolean => true/false
+    //null => stand alone value
+    //undefined 
+    //symbol => unique
+
+//2. Non premotive
+    //object => {}
+    //Array => []
+
+console.log(typeof Number("-566"))
